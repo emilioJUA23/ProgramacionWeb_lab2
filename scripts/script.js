ce6845b7ae8958a82ae679edf0966c3b8a162a76
Luis Emilio Juarez <luis_juarez2394@hotmail.com>
2018-09-06 21:53:47 -0600

    creacion de archivos por orden

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,3 @@
-
-var table_of_decks = "#decktable";
-
 function changeFont()
 {
     if (String(document.getElementById("page-top").style.fontFamily) == "Impact, Charcoal, sans-serif")
@@ -61,41 +58,6 @@ function pictureChange()
         document.getElementById('big_pic').src="images/eldrazi.png";
     }
 
-}
-
-function getSum(total, num)
-{
-    return total + num;
-}
-function calculateManaCurve()
-{
-  // obtencion de variables
-    var blueMana = parseInt(document.querySelector("#bluemana").value);
-    var redMana = parseInt(document.querySelector("#redmana").value);
-    var greenMana = parseInt(document.querySelector("#greenmana").value);
-    var blackMana = parseInt(document.querySelector("#blackmana").value);
-    var whiteMana = parseInt(document.querySelector("#whitemana").value);
-    var lands = parseInt(document.querySelector("#lands").value);
-    // calculo de pesos
-    var weights = [blueMana,redMana,greenMana,blackMana,whiteMana];
-    var sum = weights.reduce(getSum);
-    blueMana= (blueMana/sum)*lands;
-    redMana = (redMana/sum)*lands;
-    blackMana = (blackMana/sum)*lands;
-    whiteMana = (whiteMana/sum)*lands;
-    greenMana = (greenMana/sum)*lands;
-    return [blueMana,redMana,greenMana,blackMana,whiteMana,lands]
-}
-
-
-function insertDeck(data) {
-    var table = document.querySelector(table_of_decks);
-    var row = table.insertRow(table.querySelectorAll("tr").lenght);
-    for (var i in data)
-    {
-      var cell = row.insertCell(i);
-      cell.innerHTML = String(data[i]);
-    }
 }
 
 function myDeleteFunction() {
